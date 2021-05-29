6874eb862d564e598ab935848588a454a70f9572
aortiz34 <aortiz34@ucol.mx>
2021-05-29 15:59:28 -0500

    Added some things

--- main.js
+++ main.js
@@ -1,46 +1,52 @@
-function interpolacion() {
-    // num1, num2 variables will manipulate the values entered by the user
-    let x = parseFloat(document.getElementById("numberX").value);
-    let xcero = parseFloat(document.getElementById("numberXcero").value);
-    let xuno = parseFloat(document.getElementById("numberXuno").value);
-    let fx0 = parseFloat(document.getElementById("numberFx0").value);
-    let fx1 = parseFloat(document.getElementById("numberFx1").value);
-    let xdos = parseFloat(document.getElementById("numberXdos").value);
-    let fx2 = parseFloat(document.getElementById("numberFx2").value);
-    // Let's define what type of operation we have to do
-    let operation = document.getElementById("operators").value;
+let metodo = document.getElementById("metodos").value;
+if (metodo === "Método regla falsa") {
+  document.getElementById("lx0").value = "Ingrese el valor de Xl: ";
+  document.getElementById("lx1").value = "Ingrese el valor de Xu: ";
+}
+      
+function metodo() {
+    let x0 = parseFloat(document.getElementById("numberX0").value);
+    let x1 = parseFloat(document.getElementById("numberX1").value);
+    let error = parseFloat(document.getElementById("error").value);
+    let metodo = document.getElementById("metodos").value;
+    let ecuacion = document.getElementById("ecuaciones").value;
   
-    if (operation === "interLineal") {
-      document.getElementById("result").value = (
-        fx0 +
-        ((fx1 - fx0) / (xuno - xcero)) * (x - xcero)
-      ).toFixed(3);
+    if (metodo === "Método de la secante") {
+      
+      if (ecuacion === "x3 - x + 3 = 0") {
+
+      }
+      
+      if(ecuacion === "x3 + 2x2 + 10x - 20 = 0"){
+
+      }else {
+
+      }
     }
-    if (operation === "interCuadratica") {
-      let b0 = fx0;
-      let b1 = (fx1 - fx0) / (xuno - xcero);
-      let b2 = ((fx2 - fx1) / (xdos - xuno) - b1) / (xdos - xcero);
-  
-      document.getElementById("result").value = (
-        b0 +
-        b1 * (x - xcero) +
-        b2 * ((x - xcero) * (x - xuno))
-      ).toFixed(3);
+    if (metodo === "Método regla falsa") {
+     
+      if (ecuacion === "x3 - x + 3 = 0") {
+
+      }
+      
+      if(ecuacion === "x3 + 2x2 + 10x - 20 = 0"){
+
+      }else {
+        
+      }
     }
   
-    if (operation === "lagrange1") {
-      document.getElementById("result").value = (
-        ((x - xuno) / (xcero - xuno)) * fx0 +
-        ((x - xcero) / (xuno - xcero)) * fx1
-      ).toFixed(3);
-    }
-  
-    if (operation === "lagrange2") {
-      document.getElementById("result").value = (
-        ((x - xuno) / (xcero - xuno)) * ((x - xdos) / (xcero - xdos)) * fx0 +
-        ((x - xcero) / (xuno - xcero)) * ((x - xdos) / (xuno - xdos)) * fx1 +
-        ((x - xcero) / (xdos - xcero)) * ((x - xuno) / (xdos - xuno)) * fx2
-      ).toFixed(3);
+    if (metodo === "Método de bisección") {
+     
+      if (ecuacion === "x3 - x + 3 = 0") {
+
+      }
+      
+      if(ecuacion === "x3 + 2x2 + 10x - 20 = 0"){
+
+      }else {
+        
+      }
     }
   }
   
